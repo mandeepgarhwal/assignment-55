a030b9bf6702e797c999305a7fd24bc3a9d4b11d
mandeepgarhwal <114212567+mandeepgarhwal@users.noreply.github.com>
2023-09-23 05:16:17 +0530

    Update work_router.js

--- routers/work_router.js
+++ routers/work_router.js
@@ -3,8 +3,6 @@ const { fetchallwork, addnewwork, fetchonework, updatework, deleteallworks, dele
 const workrouter = express.Router()
 
 workrouter.get("/", fetchallwork)
-          .then((res) => console.log(res))
-          .catch((err) => console.log(err))
 
 workrouter.post("/", addnewwork)
 
